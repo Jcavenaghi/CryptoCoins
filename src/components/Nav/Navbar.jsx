3f00eedf0cab989a3ee4fb0a76b7b8d7885a905d
Jcavenaghi <joaquincavenaghi@gmail.com>
2023-12-22 15:00:25 -0300

    [responsive] diseño más responsive y otros cambios visuales

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -7,14 +7,14 @@ function Navbar() {
   return (
     <nav className="bg-gray-900 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-white text-2xl font-bold">
+        <Link to="/" className="text-white text-2xl font-bold hover:text-emerald-300 transition duration-200">
           CryptInfo
         </Link>
         <div>
-          <Link to="/home" className="text-white mr-4">
+          <Link to="/home" className="text-white mr-4 hover:text-emerald-300 transition duration-200">
             Home
           </Link>
-          <Link to="/trending" className="text-white mr-4">
+          <Link to="/trending" className="text-white mr-4 hover:text-emerald-300 transition duration-200">
             Trending
           </Link>
           {/* Puedes agregar más enlaces aquí según sea necesario */}
